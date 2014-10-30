f3a68ceaf35b7550a67f82630ca718fd06442bcc
Lim Jia Rong <jr.loves.maths@gmail.com>
2014-10-30 07:08:36 +0000

    added validation errors with flash message.

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,18 +8,31 @@
 module.exports = {
 
   'new':function(req,res){
+    res.locals.flash = _.clone(req.session.flash);
     res.view();
+    req.session.flash={};
   },
 
   create: function(req,res,next){
+
     //  Create a User with the params sent from
     //  the sign-up form --> new.ejs
     User.create(  req.params.all(), function userCreated (err, user){
       //  If there's an error
-      if(err) return next(err);
+      if(err) {
+        console.log(err);
+        req.session.flash = {
+          err: err
+        }
+
+        //If error redirect back to sign-up page
+        return res.redirect('/user/new');
+      }
+
       //  After successfully creating the user
       //  redirect to the show action
       res.json(user);
+      req.session.flash={};
     });
 
   }
